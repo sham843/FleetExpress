4e2c27bbc31c74dd973ca335de62816ad6fd9b15
vaishaliborate <vaishali.borate@shauryatechnosoft.com>
2022-09-06 19:01:37 +0530

    login_page_completed

---
+++ src/app/services/shared.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  codecareerPage:any;
+  constructor() { }
+  createCaptchaCarrerPage() {
+    //clear the contents of captcha div first
+    let id :any = document.getElementById('captcha');
+    id.innerHTML = "";
+
+    var charsArray =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lengthOtp = 6;
+    var captcha = [];
+    for (var i = 0; i < lengthOtp; i++) {
+    var index = Math.floor(Math.random() * charsArray.length + 1);
+    if (captcha.indexOf(charsArray[index]) == -1)
+    captcha.push(charsArray[index]);
+    else i--;
+    }
+    var canv = document.createElement("canvas");
+    canv.id = "captcha1";
+    canv.width = 120;
+    canv.height = 40;
+    var ctx:any = canv.getContext("2d");
+    ctx.font = "21px Arial";
+    ctx.fillText(captcha.join(""), 0, 30);
+    this.codecareerPage = captcha.join("");
+    let appendChild :any = document.getElementById("captcha");
+    appendChild.appendChild(canv); 
+    }
+    
+    checkvalidateCaptcha() {
+      return this.codecareerPage;
+    }
+}
