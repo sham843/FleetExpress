ac0a5cfcf6e40aee6afd2657eb114c1e25799517
stpl-mahesh <100754388+stpl-mahesh@users.noreply.github.com>
2022-12-08 14:42:19 +0530

    changes for adding routing for vehicle tracking component

--- src/app/web/web-layout/web-layout-routing.module.ts
+++ src/app/web/web-layout/web-layout-routing.module.ts
@@ -5,7 +5,9 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('../home/home.module').then(m => m.HomeModule),data: { title: ' Home' } },
   { path: 'login', loadChildren: () => import('../login/login.module').then(m => m.LoginModule), data: { title: 'Login' }, },
+  { path: 'vehicleTracking', loadChildren: () => import('../vehicle-tracking/vehicle-tracking.module').then(m => m.VehicleTrackingModule)},
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
